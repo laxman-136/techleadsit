767416d457cb529aa3819229da7e5c3d5ba86020
laxman-136 <laxman-136@users.noreply.github.com>
2026-08-13 16:50:00 +0530

    fix: Explicitly send HTTP 200 OK status header to override WordPress automatic 404 query status

--- techleadsit.php
+++ techleadsit.php
@@ -84,6 +84,9 @@ function techleadsit_route_landing_pages() {
                 $slug_inject = "\n<script>window.pageSlug = '" . esc_js($slug) . "';</script>\n";
                 $html_content = str_replace('<head>', '<head>' . $slug_inject, $html_content);
 
+                // Force HTTP status header to 200 OK (overriding WordPress automatic 404 query status)
+                status_header(200);
+
                 // Output headers and HTML content
                 header('Content-Type: text/html; charset=utf-8');
                 echo $html_content;
