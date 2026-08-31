d844136d5a1dca07c4391d1d2864e24782546e58
laxman-136 <laxman-136@users.noreply.github.com>
2026-08-31 10:28:57 +0530

    debug: Add API URL output on TeleCRM submission failures

--- techleadsit.php
+++ techleadsit.php
@@ -56,8 +56,8 @@ function techleadsit_route_landing_pages() {
                 $plugin_url = plugin_dir_url(__FILE__) . $folder_path;
                 $html_content = str_replace('href="index.css"', 'href="' . $plugin_url . 'index.css"', $html_content);
                 $html_content = str_replace('src="index.js"', 'src="' . $plugin_url . 'index.js"', $html_content);
-                $html_content = str_replace('href="styles.css"', 'href="' . $plugin_url . 'styles.css?v=10.6"', $html_content);
-                $html_content = str_replace('src="script.js"', 'src="' . $plugin_url . 'script.js?v=10.6"', $html_content);
+                $html_content = str_replace('href="styles.css"', 'href="' . $plugin_url . 'styles.css?v=10.7"', $html_content);
+                $html_content = str_replace('src="script.js"', 'src="' . $plugin_url . 'script.js?v=10.7"', $html_content);
                 $html_content = str_replace('src="logo-dark.png"', 'src="' . $plugin_url . 'logo-dark.png"', $html_content);
                 $html_content = str_replace('src="logo-light.png"', 'src="' . $plugin_url . 'logo-light.png"', $html_content);
                 $html_content = str_replace('src="images/', 'src="' . $plugin_url . 'images/', $html_content);
@@ -1080,7 +1080,7 @@ function techleadsit_handle_crm_lead(WP_REST_Request $request) {
     ));
 
     if (is_wp_error($response)) {
-        return new WP_REST_Response(array('success' => false, 'message' => 'CRM submission error: ' . $response->get_error_message()), 500);
+        return new WP_REST_Response(array('success' => false, 'message' => 'CRM submission error: ' . $response->get_error_message() . ' (URL: ' . $telecrm_api_url . ')'), 500);
     }
 
     $response_code = wp_remote_retrieve_response_code($response);
