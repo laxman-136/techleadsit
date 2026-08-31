7d451ff204d87325f84b6053634ff13209213542
laxman-136 <laxman-136@users.noreply.github.com>
2026-08-31 10:36:34 +0530

    chore: Clean up temporary test-connectivity debug route and bump cache to 11.1

--- techleadsit.php
+++ techleadsit.php
@@ -56,8 +56,8 @@ function techleadsit_route_landing_pages() {
                 $plugin_url = plugin_dir_url(__FILE__) . $folder_path;
                 $html_content = str_replace('href="index.css"', 'href="' . $plugin_url . 'index.css"', $html_content);
                 $html_content = str_replace('src="index.js"', 'src="' . $plugin_url . 'index.js"', $html_content);
-                $html_content = str_replace('href="styles.css"', 'href="' . $plugin_url . 'styles.css?v=11.0"', $html_content);
-                $html_content = str_replace('src="script.js"', 'src="' . $plugin_url . 'script.js?v=11.0"', $html_content);
+                $html_content = str_replace('href="styles.css"', 'href="' . $plugin_url . 'styles.css?v=11.1"', $html_content);
+                $html_content = str_replace('src="script.js"', 'src="' . $plugin_url . 'script.js?v=11.1"', $html_content);
                 $html_content = str_replace('src="logo-dark.png"', 'src="' . $plugin_url . 'logo-dark.png"', $html_content);
                 $html_content = str_replace('src="logo-light.png"', 'src="' . $plugin_url . 'logo-light.png"', $html_content);
                 $html_content = str_replace('src="images/', 'src="' . $plugin_url . 'images/', $html_content);
@@ -708,32 +708,7 @@ add_action('rest_api_init', function () {
         'callback' => 'techleadsit_handle_verify_otp',
         'permission_callback' => '__return_true'
     ));
-    register_rest_route('techleadsit/v1', '/test-connectivity', array(
-        'methods' => 'GET',
-        'callback' => 'techleadsit_handle_test_connectivity',
-        'permission_callback' => '__return_true'
-    ));
 });
-
-function techleadsit_handle_test_connectivity(WP_REST_Request $request) {
-    $results = array();
-    $api_key = defined('TELECRM_API_KEY') ? TELECRM_API_KEY : ''; 
-    $urls = array(
-        'google' => 'https://www.google.com',
-        'api_telecrm' => 'https://api.telecrm.in/api/v1/leads',
-        'app_telecrm' => 'https://app.telecrm.in/api/b1/enterprise/' . $api_key . '/autoupdatelead',
-        'next_telecrm' => 'https://next.telecrm.in/api/b1/enterprise/' . $api_key . '/autoupdatelead'
-    );
-    foreach ($urls as $name => $url) {
-        $response = wp_remote_get($url, array('timeout' => 5));
-        if (is_wp_error($response)) {
-            $results[$name] = 'Error: ' . $response->get_error_message();
-        } else {
-            $results[$name] = 'Success: Code ' . wp_remote_retrieve_response_code($response);
-        }
-    }
-    return new WP_REST_Response($results, 200);
-}
 
 /**
  * Resolves geolocation data array (City, State, Country) from a given IP address using ip-api.com
